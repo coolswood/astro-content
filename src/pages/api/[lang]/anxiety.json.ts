d0a35a8f5d1f24554d6adca94ca7d6e11f8d867e
coolswood <coolswood@yandex.ru>
2025-07-21 15:05:33 +0300

    anx

--- src/pages/api/[lang]/anxiety.json.ts
+++ src/pages/api/[lang]/anxiety.json.ts
@@ -2,7 +2,6 @@
 import type { APIRoute, GetStaticPaths } from 'astro';
 import fs from 'fs/promises';
 import path from 'path';
-import { text } from 'stream/consumers';
 
 export const prerender = true;
 
@@ -17,19 +16,24 @@ export const getStaticPaths: GetStaticPaths = async () => {
   return paths;
 };
 
+const revItems = new Set([4, 8, 12, 16, 18]);
+
 export const GET: APIRoute = async ({ params }) => {
   const lang = params.lang!;
 
   try {
     const data = JSON.parse(
       await fs.readFile(
-        path.resolve(`src/i18n/${lang}/tests/bdi.json`),
+        path.resolve(`src/i18n/${lang}/tests/anxiety.json`),
         'utf-8',
       ),
     );
 
     const en = JSON.parse(
-      await fs.readFile(path.resolve(`src/i18n/en/tests/bdi.json`), 'utf-8'),
+      await fs.readFile(
+        path.resolve(`src/i18n/en/tests/anxiety.json`),
+        'utf-8',
+      ),
     );
 
     const constants = JSON.parse(
@@ -39,41 +43,49 @@ export const GET: APIRoute = async ({ params }) => {
       ),
     );
 
+    const options = [
+      data.variants.never,
+      data.variants.seldom,
+      data.variants.often,
+      data.variants.always,
+    ];
+
     const output = {
-      title: constants.bdi,
-      steps: Object.keys(data.steps).map((key: string) => ({
-        g: null,
-        v: data.steps[key].map((text: string, i: number) => ({
+      title: constants.anxiety,
+      steps: data.steps.map((key: string, index: number) => ({
+        g: key,
+        v: options.map((text: any, i: number) => ({
           t: text,
-          s: i,
+          s: revItems.has(index) ? 4 - i : i + 1,
         })),
       })),
       result: [
         {
           text: data.result.normal.text,
           title: data.result.normal.title,
-          rangeBefore: 13,
+          rangeBefore: 44,
         },
         {
-          text: data.result.mild.text,
-          title: data.result.mild.title,
-          rangeBefore: 24,
+          text: data.result.light.text,
+          title: data.result.light.title,
+          rangeBefore: 59,
         },
         {
-          text: data.result.moderate.text,
-          title: data.result.moderate.title,
-          rangeBefore: 40,
+          text: data.result.severe.text,
+          title: data.result.severe.title,
+          rangeBefore: 74,
         },
         {
-          text: data.result.exacerbated.text,
-          title: data.result.exacerbated.title,
-          rangeBefore: 63,
+          text: data.result.extreme.text,
+          title: data.result.extreme.title,
+          rangeBefore: 80,
         },
       ],
       steps_description: Object.values(data.steps_description ?? {}),
       description: {
         text: data.description.text,
-        sourceLink: 'https://en.wikipedia.org/wiki/Beck_Depression_Inventory',
+        sourceLink:
+          'https://en.wikipedia.org/wiki/Zung_Self-Rating_Anxiety_Scale',
       },
       recommends: {
         activities: [
