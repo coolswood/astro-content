54c393a3fe46a15b8ee30ff0bc89d8c0d8bef770
coolswood <coolswood@yandex.ru>
2025-07-24 12:36:14 +0300

    fix

---
+++ src/pages/api/mindhealth/[lang]/story/depression/[story]-short.json.ts
@@ -0,0 +1,50 @@
+// src/pages/api/[lang]/[test].json.ts
+import type { APIRoute } from 'astro';
+import fs from 'fs/promises';
+import path from 'path';
+
+export const prerender = true;
+
+export async function getStaticPaths() {
+  const langs = await fs.readdir('src/i18n');
+
+  const nestedPaths = await Promise.all(
+    langs.map(async (lang) => {
+      const stories = await fs.readdir(`src/i18n/${lang}/story/depression`);
+      return stories.map((story) => ({
+        params: { lang, story: story.replace('.json', '') },
+      }));
+    }),
+  );
+
+  return nestedPaths.flat();
+}
+
+export const GET: APIRoute = async ({ params }) => {
+  const lang = params.lang!;
+  const story = params.story!;
+
+  try {
+    const s = JSON.parse(
+      await fs.readFile(
+        path.resolve(`src/i18n/${lang}/story/depression/${story}.json`),
+        'utf-8',
+      ),
+    );
+
+    const output = {
+      title: s.title,
+      img: story,
+      isPremium: s.isPremium,
+    };
+
+    return new Response(JSON.stringify(output), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (err) {
+    return new Response(
+      JSON.stringify({ error: 'Not found or broken test file' }),
+      { status: 404 },
+    );
+  }
+};
